c58c601496c6da988ccf39da73da535f3de68d2c
aydanyunus <aydanyunus98@gmail.com>
2024-01-09 23:52:05 +0400

    update style

--- src/App.tsx
+++ src/App.tsx
@@ -50,24 +50,27 @@ const ChatRoom = () => {
         text: value,
         uid,
         photoURL,
-        createdAt: serverTimestamp()
+        createdAt: serverTimestamp(),
       });
 
-      setValue('')
+      setValue("");
     }
   };
   return (
     <>
       {messages && messages?.map((m) => <ChatMessage msg={m} key={m.id} />)}
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className="fixed bottom-0 flex w-2/4">
         <input
           type="text"
           value={value}
           onChange={(e) => setValue(e.target.value)}
           placeholder="type here..."
+          className="bg-[#3a3a3a] text-white outline-none w-full py-4 px-2"
         />
 
-        <button type="submit">submit</button>
+        <button type="submit" className="bg-[#38388f] text-white px-2">
+          submit
+        </button>
       </form>
     </>
   );
@@ -85,17 +88,22 @@ const SignIn = () => {
 const SignOut = () => {
   return (
     getAuth().currentUser && (
-      <button onClick={() => signOut(auth)}>sign out</button>
+      <button
+        onClick={() => signOut(auth)}
+        className="bg-[#282c34] text-white p-3"
+      >
+        sign out
+      </button>
     )
   );
 };
 
-const ChatMessage = ({msg}: any) => {
+const ChatMessage = ({ msg }: any) => {
   const { text, photoURL } = msg;
   return (
-    <div>
-      <img src={photoURL} />
-      <p>{text}</p>
+    <div className="px-2 flex mb-6 items-center gap-3">
+      <img src={photoURL} className="rounded-full w-16 h-16" />
+      <p className="text-white">{text}</p>
     </div>
   );
 };
@@ -104,12 +112,17 @@ function App() {
   const [user] = useAuthState(auth);
 
   return (
-    <>
-      <header>
-        <SignOut />
-      </header>
-      <section>{user ? <ChatRoom /> : <SignIn />}</section>
-    </>
+    <div className="bg-[#282c34]">
+      <div className="w-2/4 mx-auto">
+        <header className="py-4 px-2 bg-black  text-white flex justify-between items-center">
+          <h1 className="text-2xl">🚀👾🗯️</h1>
+          <SignOut />
+        </header>
+        <section className="bg-[#282533] min-h-screen py-4 overflow-y-scroll">
+          {user ? <ChatRoom /> : <SignIn />}
+        </section>
+      </div>
+    </div>
   );
 }
 
